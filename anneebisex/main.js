6b01dfeef01009e7f2efa32ebec2056db646cafc
YoannGenique <genique.yoann@outlook.com>
2021-02-25 11:21:59 +0100

    annee

---
+++ anneebisex/main.js
@@ -0,0 +1,10 @@
+const check = document.getElementById("check") ;
+
+check.addEventListener("click", function() {
+    let year = document.getElementById("year").value;
+    if(year % 400 === 0 || year % 4 === 0 && year % 100 !== 0){
+        result.innerHTML='L\'année '+year+' est Bisextile.';
+    }else{
+        result.innerHTML='L\'année '+year+' n\'est pas Bisextile.';
+    }
+});
--- convertisseurunitedetemps/main.js
+++ convertisseurunitedetemps/main.js
@@ -6,10 +6,12 @@ const daysInYear = 365;
 
 convert.addEventListener("click", function() {
     let numberOfYears = document.getElementById("numberOfYears").value ;
+
     let days = daysInYear*numberOfYears;
     let hour = days*hoursInDay;
     let minute = hour*minutesInHour;
     let second = minute*secondsInMinute;
+    
     resultDays.innerHTML='Dans '+numberOfYears+ ' ans il y a ' +days+ ' jours.';
     resultHours.innerHTML='Dans '+numberOfYears+ ' ans il y a ' +hour+ ' heure.';
     resultMinutes.innerHTML='Dans '+numberOfYears+ ' ans il y a ' +minute+ ' minutes.';
