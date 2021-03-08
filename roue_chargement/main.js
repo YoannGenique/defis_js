dba868b904033ee595528116e01fbed1522bbee0
Yoann Génique <genique.yoann@outlook.com>
2021-03-08 09:42:38 +0100

    maj

--- roue_chargement/main.js
+++ roue_chargement/main.js
@@ -5,11 +5,11 @@ window.onload = showContent();
 
 
 function showContent() {
-  // Pour déclencher les 3 secondes
+
   setTimeout(function(){
-  // Disparition de la div loadingAnimation
+
   loading.style.display = 'none';
-  // Apparition de la div displayContent
   content.style.display = 'block';
+  
   }, 3000);
 }
